6fd1afa8559bdd3d3b18f65b07f65fd86058a51c
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2020-10-05 22:50:34 -0300

    tirando o -y dos links

--- tudo/script do apps.sh
+++ tudo/script do apps.sh
@@ -45,21 +45,21 @@ sudo snap install keepassxc ; vlc ; natron ; homeserver -y
 
 cd /home/$USER/Downloads/apps-para-baixar-no-linux/arquivos-deb
 
-wget -c https://az764295.vo.msecnd.net/stable/2af051012b66169dde0c4dfae3f5ef48f787ff69/code_1.49.3-1601661857_amd64.deb -y
+wget -c https://az764295.vo.msecnd.net/stable/2af051012b66169dde0c4dfae3f5ef48f787ff69/code_1.49.3-1601661857_amd64.deb 
 
-wget -c https://r1---sn-up4p8xcg-cnce.gvt1.com/edgedl/android/studio/ide-zips/4.0.1.0/android-studio-ide-193.6626763-linux.tar.gz?cms_redirect=yes&mh=Ft&mip=177.66.167.12&mm=28&mn=sn-up4p8xcg-cnce&ms=nvh&mt=1601946677&mv=m&mvi=1&pcm2cms=yes&pl=24&shardbypass=yes -y
+wget -c https://r1---sn-up4p8xcg-cnce.gvt1.com/edgedl/android/studio/ide-zips/4.0.1.0/android-studio-ide-193.6626763-linux.tar.gz?cms_redirect=yes&mh=Ft&mip=177.66.167.12&mm=28&mn=sn-up4p8xcg-cnce&ms=nvh&mt=1601946677&mv=m&mvi=1&pcm2cms=yes&pl=24&shardbypass=yes 
 
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -y
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 
-wegt -c https://download.virtualbox.org/virtualbox/6.1.14/virtualbox-6.1_6.1.14-140239~Ubuntu~eoan_amd64.deb -y
+wegt -c https://download.virtualbox.org/virtualbox/6.1.14/virtualbox-6.1_6.1.14-140239~Ubuntu~eoan_amd64.deb 
 
-wget -c https://repo.steampowered.com/steam/archive/precise/steam_latest.deb -y
+wget -c https://repo.steampowered.com/steam/archive/precise/steam_latest.deb 
 
 sudo dpkg -i *.deb -y
 
 
 sudo dpkg --add-architecture i386 -y
-wget -nc https://dl.winehq.org/wine-builds/winehq.key -y
+wget -nc https://dl.winehq.org/wine-builds/winehq.key 
 sudo apt-key add winehq.key -y
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' -y
 sudo apt update

60cdecc36d97404047b5bf4002e47a7a45dd65ed
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2020-10-05 22:11:51 -0300

    acrecentando apps snap

--- tudo/script do apps.sh
+++ tudo/script do apps.sh
@@ -35,12 +35,26 @@ sudo add-apt-repository ppa:audacity-team/daily
 
 sudo add-apt-repository ppa:thomas-schiex/blender
 
+sudo add-apt-repository ppa:inkscape.dev/stable
+
+sudo add-apt-repository ppa:stebbins/handbrake-releases
+
+sudo add-apt-repository ppa:kdenlive/kdenlive-stable
+
 sudo apt-get update
 
-sudao apt-get flatpak ; obs-studio ; gimp ; audacity ; blender
+sudao apt-get flatpak ; obs-studio ; gimp ; audacity ; blender ; inkscape ; handbrake-gtk ; kdenlive
+
+sudo apt update
+
+sudo snap install sublime-text --classic
+
+sudo snap install natron
+
+sudo snap install vlc
 
 
-uname -m ;
 
-wget https://downloads.natron.fr/Linux/releases/64bit/files/natron_2.3.13_amd64.deb -O natron.deb
+wget -c https://az764295.vo.msecnd.net/stable/2af051012b66169dde0c4dfae3f5ef48f787ff69/code_1.49.3-1601661857_amd64.deb 
 
+wget -c 
